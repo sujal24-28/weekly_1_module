b7b8becad98c60e97a25f418f2db6a6ab6005dcc
sujal24-28 <ersujalaktu@gmail.com>
2026-07-16 19:30:59 +0530

    modify increment Diamond pattern

--- 2. Number_patterns/6_Incrementing_Diamond_Pattern.cpp
+++ 2. Number_patterns/6_Incrementing_Diamond_Pattern.cpp
@@ -2,29 +2,44 @@
 using namespace std;
 
 int main() {
-    int start;
+    int start,end;
     cout << "Enter starting number: ";
     cin >> start;
 
-    int rows = (2 * start) - 1;
-    int mid = rows / 2;
+    cout << "Enter ending number: ";
+    cin >> end;
 
-    for (int i = 0; i < rows; i++) {
-        int num, count;
+    int up = start;
 
-        if (i <= mid) {
-            num = start + i;
-            count = i + 1;
-        } else {
-            num = start + (rows - i - 1);
-            count = rows - i;
+    for (int i = start; i <= end;i++){
+        for (int j = start; j <= i;j++){
+            cout << up; 
         }
-        for (int j = 0; j < count; j++) {
-            cout << num << " ";
-        }
+        up=up+1;
         cout << endl;
     }
 
-    return 0;
+    int down = end-1;
+    for (int i = start; i <= end-1;i++){
+        for (int j = end - 1; j >= i;j--){
+            cout << down;
+        }
+        down = down - 1;
+        cout << endl;
+    }
+        return 0;
 }
-
+// OUTPUT--
+/*
+Enter starting number: 3
+Enter ending number: 7
+3
+44
+555
+6666
+77777
+6666
+555
+44
+3
+*/
